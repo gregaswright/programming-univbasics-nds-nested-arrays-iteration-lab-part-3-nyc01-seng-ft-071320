1552c1e832a1bb9535dcb2f2989b02d7006f6358
Gregory A.S. Wright <geslim@gmail.com>
2020-06-29 17:29:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,15 @@
 def join_nested_strings(src)
-src.join(" ")
-
-src
+  join_string = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index] do
+      if src[row_index][element_index] == String
+        join_string.push(src[row_index][element_index])
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
+  join_string
 end
