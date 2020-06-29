ad0b35a13e2e65688567ed71b2124f216e73380a
Gregory A.S. Wright <geslim@gmail.com>
2020-06-29 16:46:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def join_nested_strings(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      if src[element_index][row_index] < 300
+      if src[element_index][row_index] == String
         delete[row_index]
       end
       element_index += 1
